6cad0a40f5575dd5f7f381f81053830343bda48d
Prabhat Garg <prabhat219@outlook.com>
2021-04-07 12:22:53 +0530

    Two

---
+++ HagitageWoodsMVC/Controllers/ProductsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HagitageWoodsMVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HagitageWoodsMVC.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly HeritageWoodsContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductsController(HeritageWoodsContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var heritageWoodsContext = _context.Products.Include(p => p.C);
+            return View(await heritageWoodsContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Include(p => p.C)
+                .FirstOrDefaultAsync(m => m.Pid == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["Catname"] = new SelectList(_context.ProductCategory, "Catname", "Catname");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Pid,Cid,Name,Description,Price,Stock,ProductImage,ProductPic")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                string rootPath = _webHostEnvironment.WebRootPath;
+                string fileName = Path.GetFileName(products.ProductPic.FileName);
+                string pPath = Path.Combine(rootPath + "/Images/" + fileName);
+                products.ProductImage = fileName;
+                var filStream = new FileStream(pPath, FileMode.Create);
+                await products.ProductPic.CopyToAsync(filStream);
+
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Catname"] = new SelectList(_context.ProductCategory, "Catname", "Catname", products.Cid);
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            ViewData["Cid"] = new SelectList(_context.ProductCategory, "Cid", "Cid", products.Cid);
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Pid,Cid,Name,Description,Price,Stock,ProductImage")] Products products)
+        {
+            if (id != products.Pid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.Pid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Cid"] = new SelectList(_context.ProductCategory, "Cid", "Cid", products.Cid);
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Include(p => p.C)
+                .FirstOrDefaultAsync(m => m.Pid == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.Pid == id);
+        }
+    }
+}
